612f6b07644d1a8be871544ce5178a646338101e
root <root@vps238285.ovh.net>
2016-01-20 18:16:51 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+/*
+Файл конфугираций by Islam [ДикиЙ]
+ */
+ 
+ 
+var config = {
+  site: 'http://cron.alexx876.xyz/',  //Стандартный домен
+  token: '',    //Ваш токен
+  komands: 'https://vk.com/id341448317?w=wall341448317_2%2Fall' //Ссылка на страницу с командами
+
+	}
+
+module.exports = config;	
